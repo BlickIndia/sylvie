b0c7cb0ffaf32fb06e94de465ac4723b42937e88
Mauricio Ivan Mejía Ramirez <mauriciodinki@gmail.com>
2017-01-08 20:05:19 -0600

    Update 2017-01-09T02:05:06.674Z

--- gulptasks/copy.fonts.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp'),
-  paths = require('../gulpfile.config').paths;
-
-gulp.task('copy:fonts', function () {
-  gulp.src(paths.src.fonts)
-    .pipe(gulp.dest(paths.dist.fonts));
-});
